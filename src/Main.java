753c5a2abc878f57e44611599eac33ed1baa518d
NScott65 <144457750+NScott65@users.noreply.github.com>
2023-11-10 14:35:36 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int userNum;
+        int userNum = 0;
         int sum = 0;
+
 
         System.out.println("Enter all of your numbers: ");
         if(scan.hasNextInt()){
             userNum = scan.nextInt();
-            for(int i = 0; i < userNum; i++){
-                int s = userNum % 10;
-                int p = s / 10;
-                 sum = sum + p;
+            for(; userNum != 0; userNum /= 10){
+                sum += userNum % 10;
             }
             System.out.println("The sum is: " + sum);
         }
