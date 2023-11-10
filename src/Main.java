3ad0d05904aea9ea2373f02b924a556077aa485b
Noah <scott.613865@swl.k12.oh.us>
2023-11-10 09:46:11 -0500

    hi

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        int userNum;
+        int sum = 0;
+
+        System.out.println("Enter all of your numbers: ");
+        if(scan.hasNextInt()){
+            userNum = scan.nextInt();
+            for(int i = 0; i < userNum; i++){
+                int s = userNum % 10;
+                int p = s / 10;
+                 sum = sum + p;
+            }
+            System.out.println("The sum is: " + sum);
+        }
     }
 }
